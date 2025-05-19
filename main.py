b82ded669e6bdd7cc94734b11512a50ff0153ca3
rodrig20 <rodrigo.f.lopes2004@gmail.com>
2025-05-19 13:11:59 +0100

    Add option to start without a WhatsApp 64-digit key and instead use only the name to find the file’s creation date

--- main.py
+++ main.py
@@ -5,23 +5,38 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="WhatsApp Date Fix")
-    parser.add_argument("key", type=str, help="Your 64-digit key")
+    parser.add_argument(
+        "key", type=str, nargs="?", default=None, help="Your 64-digit key"
+    )
 
     args = parser.parse_args()
     print("Connect your device...")
     subprocess.run("adb wait-for-device".split(), check=True)
-    # Transfer the WhatsApp database to the computer
-    subprocess.run("adb pull /storage/emulated/0/Android/media/com.whatsapp/WhatsApp .".split(), check=True)
+    if args.key is not None:
+        # Transfer the WhatsApp database to the computer
+        subprocess.run(
+            "adb pull /storage/emulated/0/Android/media/com.whatsapp/WhatsApp .".split(),
+            check=True,
+        )
 
-    subprocess.run(f"wtsexporter -a -k {args.key} -b ./WhatsApp/Databases/msgstore.db.crypt15 --json --per-chat --no-html".split(), check=True)
+        subprocess.run(
+            f"wtsexporter -a -k {args.key} -b ./WhatsApp/Databases/msgstore.db.crypt15 --json --per-chat --no-html".split(),
+            check=True,
+        )
 
     print("\n\nStarting...")
     images = Info("image")
-    images.update_all()
+    if args.key is not None:
+        images.update_all()
+    else:
+        images.update_from_name()
     del images
 
     videos = Info("video")
-    videos.update_all()
+    if args.key is not None:
+        videos.update_all()
+    else:
+        videos.update_from_name()
     del videos
 
     Info.clear()
